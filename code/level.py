54ad6a2cb531bc5cf586ab2663f1c4a9d4febfb3
bimal-creatives <72511459+bimal-creatives@users.noreply.github.com>
2022-04-10 19:34:30 +0530

    failed

--- code/level.py
+++ code/level.py
@@ -1,47 +1,52 @@
 from numpy import tile
 import pygame
 from player import Player
-from tiles import Tile 
+from tiles import Tile
 from settings import tile_size
 
 
 class Level:
-    def __init__(self,level_data,surface):
+    def __init__(self, level_data, surface):
 
         self.display_surface = surface
         self.setup_level(level_data)
-
         self.world_shift = 0
 
-    def setup_level(self,layout):
+    def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
         self.player = pygame.sprite.GroupSingle()
 
-        for row_index,row in enumerate(layout):
-            for col_index,cell in enumerate(row):
-                if cell == 'X':
-				
-                    x = col_index * tile_size
-                    y = row_index * tile_size
-                    tile = Tile((x,y),tile_size)
-                    self.tiles.add(tile)
+        for row_index, row in enumerate(layout):
+            for col_index, cell in enumerate(row):
+                x = col_index * tile_size
+                y = row_index * tile_size
 
+                if cell == 'X':
+                    tile = Tile((x, y), tile_size)
+                    self.tiles.add(tile)
                 if cell == 'P':
-                    player_sprite = Player((x,y))
+                    player_sprite = Player((x, y), self.display_surface)
                     self.player.add(player_sprite)
-					
-					
-                    
-        
+
+    def scroll_x(self):
+        player = self.player.sprite
+        player_x = player.rect.centerx
+        direction_x = player.direction.x
+
+        if player_x < 200 and direction_x < 0:
+            self.world_shift = 8
+            player.speed = 0
+        elif player_x > 1000 and direction_x > 0:
+            self.world_shift = -8
+            player.speed = 0
+        else:
+            self.world_shift = 0
+            player.speed = 8
 
     def run(self):
         self.tiles.update(0)
         self.tiles.draw(self.display_surface)
-        #self.player.draw(self.display_surface)
-		
-		
-	
-    
 
-    
-
+        self.player.update()
+        self.player.draw(self.display_surface)
+        self.scroll_x()
