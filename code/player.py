3322ca123f9bad11659e28212d957f361ebaa474
bimal-creatives <72511459+bimal-creatives@users.noreply.github.com>
2022-04-10 10:15:16 +0530

    module problem solved

--- code/player.py
+++ code/player.py
@@ -1,8 +1,9 @@
 import pygame 
+from pygame.surface import Surface
 
 class Player(pygame.sprite.Sprite):
     def __init__(self,pos):
         super().__init__()
-        self.image = pygame.surface((32,64))
+        self.image = Surface((32,64))
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft = pos)
